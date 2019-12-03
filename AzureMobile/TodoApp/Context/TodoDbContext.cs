935de250c5388bb8b500cd56b15fad2d3e1971f1
rameshksh <ramesh.sharma12@gmail.com>
2019-12-03 18:46:51 +0530

    Working android app

---
+++ AzureMobile/TodoApp/Context/TodoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApp.Models;
+
+namespace TodoApp.Context
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ AzureMobile/TodoApp/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
